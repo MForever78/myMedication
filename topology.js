cec9f6cdf0d0cc1b08c925daf2546f196f04a0b4
MForever78 <shunjian1128@gmail.com>
2014-09-09 17:54:21 +0800

    Add basic test suite

--- topology.js
+++ topology.js
@@ -11,6 +11,12 @@ app.set('view engine', 'jade');
 // set default port: 3000
 app.set('port', process.env.PORT || 3000);
 
+// catch tests
+app.use(function(req, res, next) {
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  next();
+});
+
 // start the server
 app.listen(app.get('port'), function(){
   console.log( 'Express started listiening on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
