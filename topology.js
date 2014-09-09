e825166cc6dee75c7080a590e7ccee58e45cfd22
MForever78 <shunjian1128@gmail.com>
2014-09-09 22:46:19 +0800

    Add logger support

--- topology.js
+++ topology.js
@@ -11,14 +11,29 @@ app.set('view engine', 'jade');
 // set default port: 3000
 app.set('port', process.env.PORT || 3000);
 
+// set static directory
+app.use(express.static(__dirname + '/public'));
+
 // catch tests
 app.use(function(req, res, next) {
   res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
   next();
 });
 
+// set logger according to runtime env
+switch(app.get('env')) {
+  case 'development':
+    app.use(require('morgan')('dev'));
+    break;
+  case 'production':
+    app.use(require('express-logger')({
+      path: __dirname + '/log/requests.log'
+    }));
+    break;
+}
+
 // start the server
 app.listen(app.get('port'), function(){
-  console.log( 'Express started listiening on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
+  console.log( 'Express started in ' + app.get('env') + ' mode on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
 });
 
