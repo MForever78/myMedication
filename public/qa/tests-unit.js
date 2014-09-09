ccba1837b9719c19ac10c1f7ea0c3e48c280890b
MForever78 <shunjian1128@gmail.com>
2014-09-09 21:39:17 +0800

    rename asset to assets to meet standards

---
+++ public/qa/tests-unit.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+
+// Example test
+
+/*
+  suite('Function test', function() {
+    test('test a certain function', function() {
+      expect(something === otherSomething);
+    });
+  });
+*/
