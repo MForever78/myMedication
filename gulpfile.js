2a3e459142a1bf77535027e2ed3e88e9533563ca
MForever78 <shunjian1128@gmail.com>
2014-09-09 21:38:39 +0800

    Add gulp tasks: styles, scripts, images, watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,67 @@
-var gulp = require('gulp')
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var cache = require('gulp-cache');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var jshint = require('gulp-jshint');
+var less = requrie('gulp-less');
+var livereload = require('gulp-livereload');
+var minifycss = require('gulp-minify-css');
+var notify = require('gulp-notify');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
-gulp.task('build', function() {
-  //
+gulp.task('styles', function() {
+  return gulp.src('./assets/less/main.less')
+    .pipe(less())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./public/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('./assets/js/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./public/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('images', function() {
+  return gulp.src('./assets/img/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('./public/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('clean', function(cb) {
+  del(['./public/css', './public/js', './public/img'], cb);
+});
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('styles', 'scripts', 'images');
+});
+
+gulp.task('watch', function() {
+  //Watch .less files
+  gulp.watch('./assets/less/**/*.less', ['styles']);
+
+  //Watch .js files
+  gulp.watch('./assets/js/**/*.js', ['scripts']);
+
+  //Watch image files
+  gulp.watch('./assets/img/**/*', ['images']);
+
+  //Create LiveReloaded server
+  livereload.listen();
+
+  //Watch any files in assets/, reload on change
+  gulp.watch(['./public/**']).on('change', livereload.changed);
 });
