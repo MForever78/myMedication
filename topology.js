9ae969d3bbb0735d508f4a6830c524ddc5e41a85
MForever78 <shunjian1128@gmail.com>
2014-09-13 16:27:21 +0800

    Add controllers to main

--- topology.js
+++ topology.js
@@ -1,6 +1,7 @@
 var Express = require("express");
 var jade = require('jade');
-
+var controller = require('./controllers/index');
+var credentials = require('./credentials');
 
 var app = Express();
 
@@ -24,13 +25,17 @@ app.use(function(req, res, next) {
 switch(app.get('env')) {
   case 'development':
     app.use(require('morgan')('dev'));
+    app.set('db', credentials.development.connectionString);
     break;
   case 'production':
     app.use(require('express-logger')({
       path: __dirname + '/log/requests.log'
     }));
+    app.set('db', credentials.production.connectionString);
     break;
 }
+
+controllers(app);
 
 // start the server
 app.listen(app.get('port'), function(){
