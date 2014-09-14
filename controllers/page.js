0c13970bdf3b22c4d0209077d34b2261a49e9ae9
MForever78 <shunjian1128@gmail.com>
2014-09-14 15:25:17 +0800

    modify login pages

--- controllers/page.js
+++ controllers/page.js
@@ -10,7 +10,7 @@ module.exports = {
 
   adminlogin: function(req, res, next) {
     res.render('adminlogin', {pageTitle: 'Administrator Login - Topology'});
-  }
+  },
 
   admin: function(req, res, next) {
     res.render('admin', {pageTitle: 'Administrator - Topology'});
