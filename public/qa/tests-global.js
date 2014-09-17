47a96bc00ca438b13972c5c8afed174ad403d6f7
MForever78 <shunjian1128@gmail.com>
2014-09-17 17:57:32 +0800

    use custom bootstrap variables and fix fonts path

--- public/qa/tests-global.js
+++ public/qa/tests-global.js
@@ -2,4 +2,10 @@ suite('Global Tests', function() {
   test('page has a valid title', function() {
     assert(document.title && document.title.match(/\S/));
   });
+  test('jQuery has successfully loaded', function() {
+    assert(document.title === $("title").text());
+  });
+  test('Bootstrap script has successfully loaded', function() {
+    assert($.fn.modal !== 'undefined');
+  });
 });
