b8e4cf43b6534ed8d37bf84695bf958afbd4dfe4
MForever78 <shunjian1128@gmail.com>
2014-10-19 20:26:30 +0800

    fix timetable

--- controllers/page.js
+++ controllers/page.js
@@ -7,14 +7,21 @@ module.exports = {
   signin: {
     user: function(req, res, next) {
       if (req.session._id !== undefined) {
-        res.redirect('/schedule');
+        if (req.session.type == 'Admin')
+          res.redirect('/statistic');
+        else
+          res.redirect('/schedule');
       } else {
         res.render('userSignin', {pageTitle: 'User Sign in - My Medication', bodyId: 'signin'});
       }
-  },
+    },
 
     admin: function(req, res, next) {
-      res.render('adminSignin', {pageTitle: 'Administrator Sign in - My Medication', bodyId: 'signin'});
+      if (req.session._id !== undefined && req.session.type == 'Admin') {
+        res.redirect('/statistic');
+      } else {
+        res.render('adminSignin', {pageTitle: 'Administrator Sign in - My Medication', bodyId: 'signin'});
+      }
     }
   },
 
